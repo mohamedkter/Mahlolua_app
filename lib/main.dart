bf7a55212f1874a01726c90adbcd0596a76b4b5a
abdullah011564 <bdallhbsyd4@gmail.com>
2024-06-24 05:33:34 +0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MultiBlocProvider(
       providers: [
         BlocProvider(
